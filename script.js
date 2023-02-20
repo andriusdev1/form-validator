aa0c14887692854e22eba7f6edb916ceb1e64b85
andriusdev1 <rimkunas.andrius@gmail.com>
2023-02-20 21:32:14 +0000

    Regex Fix

--- script.js
+++ script.js
@@ -30,9 +30,14 @@ function showSuccess(input) {
 // Check email is valid
 
 function isValidEmail(email) {
-    const re = / (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) /
+    const re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(string(email).toLowerCase());
 }
+
+
+
+
+
 
 
 
@@ -55,9 +60,7 @@ form.addEventListener('submit', function (e) {
         showError(email, 'Email is required');
     } else if (!isValidEmail(email.value)) {
         showError(email, 'Email is not valid');;
-    }
-
-    else {
+    } else {
         showSuccess(email);
 
     }
