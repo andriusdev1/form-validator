58cf39f66e5461e1ff3c48efd4ef4acbfbb62837
andriusdev1 <rimkunas.andrius@gmail.com>
2023-02-21 20:56:29 +0000

    Fixed Regex

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function showSuccess(input) {
 
 function isValidEmail(email) {
     const re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(string(email).toLowerCase());
+    return re.test(String(email).toLowerCase());
 }
 
 
@@ -58,13 +58,10 @@ form.addEventListener('submit', function (e) {
 
     if (email.value === '') {
         showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)) {
+    } else if(!isValidEmail(email.value)) {
         showError(email, 'Email is not valid');;
     } else {
-        showSuccess(email);
-
-    }
-
+        showSuccess(email);}
 
     if (password.value === '') {
         showError(password, 'Password is required');
